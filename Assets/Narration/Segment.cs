c911eb22596645c62f1eab0942edab65b919c209
Maxime Boudreault <maxime.boudreault@cathargames.com>
2020-11-18 18:07:13 -0500

    Remove unused script

--- Assets/Narration/Segment.cs
+++
@@ -1,9 +0,0 @@
-﻿// ©2020 Maxime Boudreault. All Rights Reserved.
-
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SoL/Segment")]
-public class Segment : ScriptableObject
-{
-    [TextArea] public string text;
-}
