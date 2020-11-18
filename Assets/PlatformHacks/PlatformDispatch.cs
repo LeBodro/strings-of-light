c21b143bb22ae5871c7639544287b51df6717cc4
Maxime Boudreault <maxime.boudreault@cathargames.com>
2020-11-18 18:32:16 -0500

    Add WebGL warning

---
+++ Assets/PlatformHacks/PlatformDispatch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlatformDispatch : MonoBehaviour
+{
+    #pragma warning disable 0414
+    [SerializeField] private int defaultSceneId = 2;
+    [SerializeField] private int webGLSceneId = 1;
+    #pragma warning restore 0414
+
+    void Awake()
+    {
+#if UNITY_WEBGL
+        SceneManager.LoadScene(webGLSceneId);
+#else
+        SceneManager.LoadScene(defaultSceneId);
+#endif
+    }
+}
---
+++ Assets/PlatformHacks/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup ui = null;
+    public void LoadScene(int index)
+    {
+        ui.interactable = false;
+        StartCoroutine(FadeTo(index));
+    }
+
+    IEnumerator FadeTo(int index)
+    {
+        const float delay = 1.67f;
+        float t = 0;
+        while (t < delay)
+        {
+            t += Time.deltaTime;
+            ui.alpha = Mathf.Lerp(1, 0, t / delay);
+            yield return null;
+        }
+
+        ui.alpha = 0;
+        SceneManager.LoadScene(index);
+    }
+}
