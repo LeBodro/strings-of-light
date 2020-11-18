58f9567529ffa7f8084718b0df313a72447d1a88
Maxime Boudreault <maxime.boudreault@cathargames.com>
2020-11-18 18:40:48 -0500

    Add fade in at game scene beginning

---
+++ Assets/FadeAndDisappear.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAndDisappear : MonoBehaviour
+{
+    void Start()
+    {
+        const float delay = 2f;
+        Image img = GetComponent<Image>();
+        img.enabled = true;
+        GetComponent<Image>().CrossFadeAlpha(0, delay, false);
+        Destroy(gameObject, delay);
+    }
+}
--- Assets/PlatformHacks/SceneLoader.cs
+++ Assets/PlatformHacks/SceneLoader.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
