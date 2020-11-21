9bdd28ff4db57844dbc70f5d581459c96656199a
Maxime Boudreault <maxime.boudreault@cathargames.com>
2020-11-21 18:18:42 -0500

    Preserve aspect ratio on thinner mobile screens

---
+++ Assets/FitToRect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FitToRect : MonoBehaviour
+{
+    [SerializeField] private Camera viewport = null;
+    [SerializeField] private RectTransform rect = null;
+    [SerializeField] private Transform[] walls = null;
+
+    void Reset()
+    {
+        viewport = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        float height = rect.rect.height;
+        float heightDelta = rect.sizeDelta.y;
+        
+        float ratio = Mathf.Abs(heightDelta / height);
+        Debug.Log(ratio);
+        viewport.orthographicSize /= 1 - ratio;
+        foreach (Transform wall in walls)
+        {
+            wall.localPosition /= 1 - ratio * 0.5f;
+        }
+    }
+}
